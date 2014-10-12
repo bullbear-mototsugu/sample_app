b621715955babd55a7c77195f2ddfd46aa124a2d
violetyk <yuhei.kagaya@gmail.com>
2014-10-13 00:56:13 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,11 +11,20 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])    # 実装は終わっていないことに注意!
+    # @user = User.new(params[:user])    # 実装は終わっていないことに注意!
+    @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う。
+      # 特殊な変数flash
+      flash[:success] = "Welcome to the Sample App!"
+      # user_urlへリダイレクトと同様
+      redirect_to @user
     else
       render 'new'
     end
+  end
+
+  def user_params
+    # strong parameters機能を使って、POST可能なパラメータを許可
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
